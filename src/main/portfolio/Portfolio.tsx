d9355c904375004357fe3dd20c4d5ca16c8046d5
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2022-07-06 15:47:02 +0300

    Portfolio section start

--- src/main/portfolio/Portfolio.tsx
+++ src/main/portfolio/Portfolio.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Project from "./project/Project";
+import Masonry from 'react-masonry-css'
 import styles from './Portfolio.module.scss'
 import fresh from '../../common/images/projects/Fresh.jpg'
 import desire from '../../common/images/projects/desire.jpg'
@@ -9,11 +10,20 @@ const Portfolio = () => {
         <section className={styles.section} id="portfolio">
             <div className="container">
                 <h2 className={styles.title+' title'}>Портфолио</h2>
-                <div className={styles.wrapper}>
+                <Masonry
+                    breakpointCols={4}
+                    className={styles.masonry}
+                    columnClassName={styles.masonryCols}>
 
-                   <Project img={fresh} title="Fresh" descr="Multi page internet shop site" stack="SCSS,GULP,JQUERY"/>
-                    <Project img={desire} title="Desire" descr="Multi page internet shop site" stack="SCSS,GULP,JQUERY"/>
-                </div>
+                    <Project img={fresh} title="Fresh" descr="Multi page internet shop site" stack="SCSS,GULP,JQUERY" height='220px'/>
+                    <Project img={desire} title="Desire" descr="Multi page internet shop site" stack="SCSS,GULP,JQUERY" height='280px'/>
+                    <Project img={desire} title="Desire" descr="Multi page internet shop site" stack="SCSS,GULP,JQUERY" height='240px'/>
+                    <Project img={desire} title="Desire" descr="Multi page internet shop site" stack="SCSS,GULP,JQUERY" height='280px'/>
+                    <Project img={desire} title="Desire" descr="Multi page internet shop site" stack="SCSS,GULP,JQUERY" height='340px'/>
+                    <Project img={desire} title="Desire" descr="Multi page internet shop site" stack="SCSS,GULP,JQUERY" height='280px'/>
+                    <Project img={desire} title="Desire" descr="Multi page internet shop site" stack="SCSS,GULP,JQUERY" height='320px'/>
+                    <Project img={desire} title="Desire" descr="Multi page internet shop site" stack="SCSS,GULP,JQUERY" height='280px'/>
+                </Masonry>
             </div>
         </section>
     );
--- src/main/portfolio/project/Project.tsx
+++ src/main/portfolio/project/Project.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import styles from './Project.module.scss'
 
-type ProjectPropsType={
-    img:string
-    title:string
-    descr?:string
-    stack?:string
-    link?:string
+type ProjectPropsType = {
+    img: string
+    title: string
+    descr?: string
+    stack?: string
+    link?: string
+    height?: string
 }
 
-const Project = (props:ProjectPropsType) => {
+const Project = (props: ProjectPropsType) => {
     return (
-        <div className={styles.projectBlock} style={{backgroundImage:`url(${props.img})`}}>
-            <h3 className={styles.title}>{props.title}</h3>
-            <p className={styles.stack}>{props.stack}</p>
-            <p className={styles.descr}>{props.descr}</p>
-            <a href={props.link} className={styles.link}>
-              Смотреть
-            </a>
+        <div className={styles.projectBlock} >
+            <div className={styles.imgBlock} style={{backgroundImage: `url(${props.img})`, minHeight: `${props.height}`}}>
+                <p className={styles.stack}>{props.stack}</p>
+                <a href={props.link} className={styles.link}>
+                    Смотреть
+                </a>
+            </div>
+            <div className={styles.info}>
+                <h3 className={styles.title}>{props.title}</h3>
+                <p className={styles.descr}>{props.descr}</p>
+
+            </div>
+
         </div>
     );
 };
