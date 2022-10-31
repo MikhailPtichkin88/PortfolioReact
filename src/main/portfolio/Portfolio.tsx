55942fc8c58bb26916d56340a8eda75f536b7f6c
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2022-10-31 23:40:22 +0300

    Update portfolio with Cards quiz app

--- src/main/portfolio/Portfolio.tsx
+++ src/main/portfolio/Portfolio.tsx
@@ -5,7 +5,7 @@ import styles from './Portfolio.module.scss'
 import fresh from '../../common/images/projects/Fresh.jpg'
 import desire from '../../common/images/projects/desire.jpg'
 import todo from '../../common/images/projects/todolist.jpg'
-import social from '../../common/images/projects/social.jpg'
+import quiz from '../../common/images/projects/quiz.jpg'
 import pulse from '../../common/images/projects/pulse.jpg'
 import paprika from '../../common/images/projects/paprika.jpg'
 import story from '../../common/images/projects/storybook.jpg'
@@ -42,32 +42,32 @@ const Portfolio = () => {
                     <ScrollAnimation animateIn={c.animate__fadeInUp} animateOnce={true}>
                     <Project img={fresh} title="Fresh" descr="Multi page internet shop site" stack="SCSS,GULP,JQUERY"
                              height='220px'
-                             link={'https://mikhail-ptichkin.info/Fresh/'}
+                             link={'https://mikhailptichkin88.github.io/Fresh/'}
                              gitHubLink={'https://github.com/MikhailPtichkin88/Fresh'}/>
                     </ScrollAnimation>
                     <ScrollAnimation animateIn={c.animate__fadeInUp} animateOnce={true} duration={1.2}>
-                    <Project img={todo} title="ToDo list" descr="React organizer app" stack="React, Redux, Typescript, unit tests, react-redux"
+                    <Project img={todo} title="ToDo list" descr="React organizer app" stack="React, Redux Toolkit, Typescript, unit tests, react-redux"
                              height='280px'
-                             link={'https://mikhailptichkin88.github.io/TodoList/'}
+                             link={'https://mikhailptichkin88.github.io/todolist/'}
                              gitHubLink={'https://github.com/MikhailPtichkin88/TodoList'}/>
                     </ScrollAnimation>
                     <ScrollAnimation animateIn={c.animate__fadeInUp} animateOnce={true} duration={1.4}>
                     <Project img={desire} title="Desire" descr="Multi page internet shop site" stack="SCSS,GULP,JQUERY"
                              height='240px'
-                             link={'https://mikhail-ptichkin.info/desire-furniture/'}
+                             link={'https://mikhailptichkin88.github.io/desire-furniture/'}
                              gitHubLink={'https://github.com/MikhailPtichkin88/Desire-furniture'}/>
                     </ScrollAnimation>
                     <ScrollAnimation animateIn={c.animate__fadeInUp} animateOnce={true} duration={1.6}>
                     <Project img={paprika} title="Healthy food" descr="Calories calculator"
                              stack="native JS without plugins" height='280px'
-                             link={'https://mikhail-ptichkin.info/food/'}
+                             link={'https://mikhailptichkin88.github.io/food/'}
                              gitHubLink={'https://github.com/MikhailPtichkin88/Food'}/>
                     </ScrollAnimation>
                     <ScrollAnimation animateIn={c.animate__fadeInUp} animateOnce={true} duration={1.8}>
-                    <Project img={social} title="Friends" descr="Social network (in process)" stack="React, Redux, Typescript, restApi"
+                    <Project img={quiz} title="Cards quiz" descr="Team project, educational app" stack="React, Redux, Typescript, restApi, Axios, Formik, React Router"
                              height='340px'
-                             link={'https://github.com/MikhailPtichkin88/social-network/tree/dev'}
-                             gitHubLink={'https://github.com/MikhailPtichkin88/social-network/tree/dev'}/>
+                             link={'https://mikhailptichkin88.github.io/cards'}
+                             gitHubLink={'https://github.com/MikhailPtichkin88/cards'}/>
                     </ScrollAnimation>
                     <ScrollAnimation animateIn={c.animate__fadeInUp} animateOnce={true} duration={2}>
                     <Project img={story} title="Storybook" descr="Custom components showcase" stack="React, Typescript, Storybook"
@@ -84,7 +84,7 @@ const Portfolio = () => {
                     <ScrollAnimation animateIn={c.animate__fadeInUp} animateOnce={true} duration={2.4}>
                     <Project img={pulse} title="Pulse" descr="Fitness band internet shop site" stack="SCSS,GULP,JQUERY"
                              height='280px'
-                             link={'https://mikhail-ptichkin.info/pulse/'}
+                             link={'https://mikhailptichkin88.github.io/pulse/'}
                              gitHubLink={'https://github.com/MikhailPtichkin88/Pulse'}/>
                     </ScrollAnimation>
                 </Masonry>
